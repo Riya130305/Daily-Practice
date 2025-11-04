9d92dc0e8826e2aad814fd497dc08a8ca049a36f
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2025-11-04 12:33:06 +0530

    Time: 52 ms (32.87%) | Memory: 46.2 MB (24.64%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,21 +1,70 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-       int n=nums.length;
-       int[] dp=new int[n];
-       Arrays.fill(dp,1);
+    //    int n=nums.length;
+    //    int[] dp=new int[n];
+    //    Arrays.fill(dp,1);
       
-       for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+    //    for(int i=1;i<n;i++){
+    //         for(int j=0;j<i;j++){
+    //             if(nums[i]>nums[j]){
+    //                 dp[i] = Math.max(dp[i], dp[j] + 1);
+    //             }
+    //         }
+    //     } 
+    //     int max=-1;
+    //     //Returing the max value from the dp array
+    //     for(int i=0;i<n;i++){
+    //         max=Math.max(max,dp[i]);
+    //     }
+    //     return max;
+    // }
+
+    // PRINTING THE ARRAY OF LIS
+    int n=nums.length;
+    int[] dp =new int[n];
+    int[] parent=new int[n];
+    Arrays.fill(dp,1);
+    for(int i=0;i<n;i++)
+    {
+        parent[i]=i;
+    }
+
+    for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            if(nums[i]>nums[j])
+            {
+                if(dp[i]<dp[j]+1)
+                {
+                    dp[i]=dp[j]+1;
+                    parent[i]=j;
                 }
             }
-        } 
-        int max=-1;
-        //Returing the max value from the dp array
-        for(int i=0;i<n;i++){
-            max=Math.max(max,dp[i]);
         }
-        return max;
+
+    }
+    //finding the max value
+    int max=-1;
+    int lastIdx=-1;
+    for(int i=0;i<n;i++)
+    {
+        if(dp[i]>max)
+        {
+            max=dp[i];
+            //strong the large element
+            lastIdx=i;
+        }
+    }
+    ArrayList<Integer> arr = new ArrayList<>();
+    arr.add(nums[lastIdx]);
+    while(parent[lastIdx]!=lastIdx)
+    {
+        arr.add(nums[parent[lastIdx]]);
+        lastIdx=parent[lastIdx];
+    }
+    Collections.reverse(arr);
+    System.out.println(arr);
+    return max;
     }
 }
