7efc7a71abe6aa7b3be7234b94b7684824001914
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2025-12-03 15:40:58 +0530

    Time: 48 ms (10.05%) | Memory: 48 MB (15.48%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> res = new ArrayList<>();
+        //max heap sort on the basis of distance 
+        PriorityQueue<int[]>pq= new PriorityQueue<>((a,b)-> {
+        if(a[1]==b[1]){
+        return b[0]-a[0];
+        }
+        else{
+        return b[1]-a[1];
+    }
+        }
+        );
+
+        for(int i=0;i<arr.length;i++)
+        {
+            int diff=Math.abs(arr[i]-x);
+            pq.offer(new int[]{arr[i],diff});
+            if(pq.size()>k)
+            {
+                pq.poll();
+            }
+        }
+        while(!pq.isEmpty())
+        {
+            res.add(pq.poll()[0]);
+        }
+        Collections.sort(res);
+        return res;
+    }
+}
