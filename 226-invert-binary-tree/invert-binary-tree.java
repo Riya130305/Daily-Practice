dbd7f2a58c7d74a94503f9053fc73dca7f0723da
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2025-12-05 10:20:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (51.98%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -15,25 +15,44 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        // Base Case
-        Queue<TreeNode> q= new LinkedList<>();
-        //TreeNode temp;
-        if(root==null)
-        return null;
-        q.add(root);
-        while(!q.isEmpty())
-        {
-            TreeNode n=q.poll();
-           //Swapping the node
-           TreeNode temp=n.left;
-           n.left=n.right;
-           n.right=temp;
+        
+        // ---------Approch 1 Level order travesal-----
+        // Queue<TreeNode> q= new LinkedList<>();
+        // //TreeNode temp;
+        // if(root==null)
+        // return null;
+        // q.add(root);
+        // while(!q.isEmpty())
+        // {
+        //     TreeNode n=q.poll();
+        //    //Swapping the node
+        //    System.out.println(n.left);
+        //    //if(left and right both are null means it is reversing null to null only )
+        //    TreeNode temp=n.left;
+        //    n.left=n.right;
+        //    n.right=temp;
 
-           if(n.left!=null)
-           q.add(n.left);
-           if(n.right!=null)
-           q.add(n.right);
-        }
+        //    if(n.left!=null)
+        //    q.add(n.left);
+        //    if(n.right!=null)
+        //    q.add(n.right);
+        // }
+        // return root;
+
+
+        //------Second Approch (RECURSION)-------
+
+          if (root == null) return null;
+
+        // swap children
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        // invert subtrees
+        invertTree(root.left);
+        invertTree(root.right);
+
         return root;
     }
 }
