bcd1a931f738ad88bcfafb973e1c82bf21ff86a8
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2026-03-20 18:12:23 +0530

    Time: 2 ms (61.60%) | Memory: 51.1 MB (38.18%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,7 +1,9 @@
 class Solution {
+    // Sliding window question
     public int numSubarraysWithSum(int[] nums, int goal) {
         return fun(nums,goal)-fun(nums,goal-1);
     }
+    // Atmost k goals 
     public int fun(int[]nums, int goal)
     {
                int i=0, j=0, sum=0, count=0;
