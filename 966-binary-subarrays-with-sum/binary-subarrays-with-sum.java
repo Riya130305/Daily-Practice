6819c066e22acc2c6aad54aac863bf8d2065ab4f
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2026-05-27 12:32:20 +0530

    Time: 1312 ms (11.74%) | Memory: 51.2 MB (28.12%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,24 +1,17 @@
 class Solution {
-    // Sliding window question
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return fun(nums,goal)-fun(nums,goal-1);
-    }
-    // Atmost k goals 
-    public int fun(int[]nums, int goal)
-    {
-               int i=0, j=0, sum=0, count=0;
-
-        while(j<nums.length)
+        int count=0;
+        for(int i=0;i<nums.length;i++)
         {
-            sum+=nums[j];
-            while( i<=j && sum>goal)
+            int sum=0;
+            for(int j=i;j<nums.length;j++)
             {
-                sum-=nums[i];
-                i++;
+                sum+=nums[j];
+                if(sum==goal){
+                    count++;
+                }
+                else if(sum>goal) break;
             }
-            count+=(j-i+1);//to get the number of subarray with particular number incresing
-            j++; 
-
         }
         return count;
     }
