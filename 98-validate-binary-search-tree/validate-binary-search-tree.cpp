f5ef48f14dde0ec703aee6f1364dda4527887a08
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2026-07-27 23:27:00 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (45.39%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -11,25 +11,47 @@
  */
 class Solution {
 public:
-    void inOrder(TreeNode* root, vector<int> &arr){
-        if(root==nullptr) return;
+    bool BST(TreeNode* root, long long &prev){
+        if(root==nullptr) return true;
 
-        inOrder(root->left,arr);
-        arr.push_back(root->val);
-        inOrder(root->right,arr);
-        
+        bool left=BST(root->left,prev);
+        if(left==0) return false;
+
+        if(root->val<=prev) return false;
+
+        prev=root->val;
+        return BST(root->right,prev);
+
     }    
 public:
     bool isValidBST(TreeNode* root) {
-        if(root==nullptr) return true;
-        vector<int> arr;
-        inOrder(root,arr);
-
-        for(int i=1;i<arr.size();i++){
-            if(arr[i-1]>=arr[i]){
-                return false;
-            }
-        }
-        return true;
+       long long prev = LLONG_MIN;
+        return BST(root,prev);
     }
 };
+
+
+// class Solution {
+// public:
+//     void inOrder(TreeNode* root, vector<int> &arr){
+//         if(root==nullptr) return;
+
+//         inOrder(root->left,arr);
+//         arr.push_back(root->val);
+//         inOrder(root->right,arr);
+        
+//     }    
+// public:
+//     bool isValidBST(TreeNode* root) {
+//         if(root==nullptr) return true;
+//         vector<int> arr;
+//         inOrder(root,arr);
+
+//         for(int i=1;i<arr.size();i++){
+//             if(arr[i-1]>=arr[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
