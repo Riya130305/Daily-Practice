a1ee24df43cbdcfbff728ab6cd8a862cf5dd62c4
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2026-07-29 12:19:01 +0530

    Time: 7 ms (5.27%) | Memory: 61.9 MB (5.28%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -9,37 +9,95 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    TreeNode* prv=nullptr;
-    TreeNode* first=nullptr;
-    TreeNode* second=nullptr;
 
-    void inOrder(TreeNode* root){
-        if(root==nullptr) return ;
+    vector<TreeNode*> inorder;
 
-        inOrder(root->left);
+    void traversal(TreeNode* root)
+    {
+        if(root==nullptr)
+            return;
 
-        if(prv!=nullptr && prv->val > root->val ){
-            if(first==nullptr){
-                first=prv;
+        traversal(root->left);
+
+        inorder.push_back(root);
+
+        traversal(root->right);
+        
+    }
+
+
+    void recoverTree(TreeNode* root) {
+
+        traversal(root);
+
+        vector<TreeNode*> sorted = inorder;
+
+        sort(sorted.begin(), sorted.end(), [](TreeNode* a, TreeNode* b) {
+        return a->val < b->val;
+        });
+
+
+        int first=-1;
+        int second=-1;
+
+
+        for(int i=0;i<inorder.size();i++)
+        {
+            if(inorder[i]!=sorted[i])
+            {
+                if(first==-1)
+                    first=i;
+                else
+                    second=i;
             }
-            second=root;
         }
 
-        prv=root;
-        inOrder(root->right);
-    }    
 
-public:
-    void recoverTree(TreeNode* root) {
-        if(root==nullptr) return ;
-        
-        inOrder(root);
-
-        // swap logic
-        int temp= first->val;
-        first->val=second->val;
-        second->val=temp;
+        swap(inorder[first]->val,inorder[second]->val);
     }
 };
+
+// T.C -> Traversal O(n) Sorting O(n log n) --> O(nlog(n))
+// S.C -> O(n);
+
+
+// class Solution {
+// public:
+//     TreeNode* prv=nullptr;
+//     TreeNode* first=nullptr;
+//     TreeNode* second=nullptr;
+
+//     void inOrder(TreeNode* root){
+//         if(root==nullptr) return ;
+
+//         inOrder(root->left);
+
+//         if(prv!=nullptr && prv->val > root->val ){
+//             if(first==nullptr){
+//                 first=prv;
+//             }
+//             second=root;
+//         }
+
+//         prv=root;
+//         inOrder(root->right);
+//     }    
+
+// public:
+//     void recoverTree(TreeNode* root) {
+//         if(root==nullptr) return ;
+        
+//         inOrder(root);
+
+//         // swap logic
+//         int temp= first->val;
+//         first->val=second->val;
+//         second->val=temp;
+//     }
+// };
+
+// //T.C -> O(N)
+// //S.C -> O(H)
