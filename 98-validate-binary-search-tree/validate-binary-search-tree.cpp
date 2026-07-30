b17a8a3f32a751b140a19b6d82a223f9c8b586b8
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2026-07-30 12:25:52 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (74.91%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,57 +1,40 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
+
+    TreeNode* prev = nullptr;
+
 public:
-    bool BST(TreeNode* root, long long &prev){
-        if(root==nullptr) return true;
 
-        bool left=BST(root->left,prev);
-        if(left==0) return false;
+    bool inOrder(TreeNode* root){
 
-        if(root->val<=prev) return false;
+        if(root == nullptr)
+            return true;
 
-        prev=root->val;
-        return BST(root->right,prev);
 
-    }    
-public:
-    bool isValidBST(TreeNode* root) {
-       long long prev = LLONG_MIN;
-        return BST(root,prev);
+        // left
+        if(!inOrder(root->left))
+            return false;
+
+
+        // current
+        if(prev != nullptr && root->val <= prev->val)
+            return false;
+
+
+        prev = root;
+
+
+        // right
+        if(!inOrder(root->right))
+            return false;
+
+
+        return true;
+    }
+
+
+    bool isValidBST(TreeNode* root){
+
+        return inOrder(root);
+
     }
 };
-
-
-// class Solution {
-// public:
-//     void inOrder(TreeNode* root, vector<int> &arr){
-//         if(root==nullptr) return;
-
-//         inOrder(root->left,arr);
-//         arr.push_back(root->val);
-//         inOrder(root->right,arr);
-        
-//     }    
-// public:
-//     bool isValidBST(TreeNode* root) {
-//         if(root==nullptr) return true;
-//         vector<int> arr;
-//         inOrder(root,arr);
-
-//         for(int i=1;i<arr.size();i++){
-//             if(arr[i-1]>=arr[i]){
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-// };
