2f81b951c36fa17fee49280466f2a4af335723de
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2026-08-17 19:01:27 +0530

    Time: 158 ms (5.25%) | Memory: 46.6 MB (5.59%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,18 +1,13 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.charAt(0)==']' || s.charAt(0)=='}'|| s.charAt(0)==')'){
-        return false;}
-        ArrayDeque<Character> st = new ArrayDeque<>();
-        for(int i=0;i<s.length();i++){
 
-            if(s.charAt(i)=='[' || s.charAt(i)=='(' || s.charAt(i)=='{')  st.push(s.charAt(i));
-            else if(!st.isEmpty() &&( st.peek()=='[' && s.charAt(i)==']')) st.pop();
-            else if(!st.isEmpty() &&( st.peek()=='(' && s.charAt(i)==')')) st.pop();
-            else if(!st.isEmpty() &&( st.peek()=='{' && s.charAt(i)=='}')) st.pop();
-            else return false;
+        while(s.contains("()") || s.contains("{}") || s.contains("[]")) {
+
+            s = s.replace("()", "")
+                 .replace("{}", "")
+                 .replace("[]", "");
         }
-        if(st.isEmpty()) return true;
-        return false;
 
+        return s.length() == 0;
     }
 }
