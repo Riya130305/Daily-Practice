17b4cd22d3fee00620918d1f4cd6d808df7057dd
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2026-08-20 17:46:47 +0530

    Time: 29 ms (7.18%) | Memory: 47.6 MB (94.58%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,31 +1,27 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        int end=Arrays.stream(piles).max().getAsInt();
-        int start=1;
+    public int minEatingSpeed(int[] arr, int h) {
+        int e=Arrays.stream(arr).max().getAsInt();
+        int s=1;
+        int ans=0;
 
-        while(start<=end)
-        {
-            int mid=start+(end-start)/2;
-            if(fun(piles,mid)<=h)
-            {
-                end=mid-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(fun(arr,h,mid)<=h){
+                // ans=mid;
+                e=mid-1;
             }
-            else
-            {
-                start=mid+1;
+            else{
+                s=mid+1;
             }
-
         }
-        return start;
+        return s;
     }
-
-    public static int fun(int[]piles,int mid)
-    {
+    public int fun(int[] arr, int h, int mid){
         int sum=0;
-        for(int i=0;i<piles.length;i++)
-        {
-            sum+=Math.ceil((double)piles[i]/mid);
+       
+        for(int i=0;i<arr.length;i++){
+            sum+=Math.ceil((double)arr[i]/mid);
         }
         return sum;
-    }
+    } 
 }
