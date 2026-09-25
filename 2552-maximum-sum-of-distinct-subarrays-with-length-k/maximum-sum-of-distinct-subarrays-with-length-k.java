2243811f1f3bbc55ed8b69553696836ddec643ed
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2026-09-25 22:29:23 +0530

    Time: 55 ms (44.05%) | Memory: 99.5 MB (13.86%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,45 +1,28 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
+        long max=0,sum=0;
+        int i=0,j=0;
+        HashMap<Integer,Integer> map = new HashMap<>();
 
-        long sum = 0;
-        long max = 0;
+        while(j<nums.length){
+            sum+=nums[j];
+            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
 
-        int i = 0, j = 0;
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        while (j < nums.length) {
-
-            // 1. Add incoming element
-            sum += nums[j];
-            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
-
-            // 2. If window exceeds k, remove from left
-            if (j - i + 1 > k) {
-
-                sum -= nums[i];
-
-                map.put(nums[i], map.get(nums[i]) - 1);
-
-                if (map.get(nums[i]) == 0) {
+            if(j-i+1>k){
+                sum-=nums[i];
+                map.put(nums[i],map.get(nums[i])-1);
+                if(map.get(nums[i])==0) {
                     map.remove(nums[i]);
                 }
-
+               
                 i++;
             }
-
-            // 3. Window is exactly k
-            if (j - i + 1 == k) {
-
-                // only valid if all elements are distinct
-                if (map.size() == k) {
-                    max = Math.max(max, sum);
-                }
-            }
-
+            if(j-i+1==k){
+            if(map.size()==k){
+                max=Math.max(max,sum);
+            }}
             j++;
         }
-
         return max;
     }
 }
